41e67938e7190807901e122408f1870e700aae2b
Nikola <nikovskin@gmail.com>
2025-06-21 11:10:24 +0200

    hero section

--- front-page.php
+++ front-page.php
@@ -1,11 +1,5 @@
 <?php get_header(); ?>
 
-<?php
-// Render the hero section BEFORE the content and scripts
-if (is_page(['konstituenti', 'studenti', 'gragjani', 'sorabotnici'])) {
-    get_template_part('template-parts/hero');
-}
-?>
 
 <?php if (have_posts()):
     while (have_posts()):
